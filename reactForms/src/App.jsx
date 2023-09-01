9e193914548dd74e1765de3812b00ea617471189
Mathew <mathewlmcdowell@gmail.com>
2023-08-31 20:47:00 -0500

    yes

--- reactForms/src/App.jsx
+++ reactForms/src/App.jsx
@@ -1,10 +1,15 @@
-import React from "react"
+import Authenticate from "./components/Authenticate"
+import SignUpForm from "./components/SignUpForm"
+import { useState } from "react"
 
 export default function App() {
 
+  const [token, setToken] = useState(null);
+
   return(
-    <div>
-      
+    <div className="center">
+      <SignUpForm token={token} setToken={setToken} />
+      <Authenticate token={token} setToken={setToken} />
     </div>
-  )
+  );
 }
